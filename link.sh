5ce6c50ec4482d14f70dd3ce6ea687d9b359d8be
Kett Lovahr <catusfelony@gmail.com>
2023-08-29 18:37:46 -0300

    Added simple script for automatically linking files

---
+++ link.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+function link() {
+    SOURCE="$1"
+    TARGET="$2"
+    NAME="$3"
+    echo ":: Linking $NAME configuration"
+    if [[ -L "$TARGET" ]]; then
+        echo "!! $NAME configuration is already a link, unlinking"
+        unlink "$TARGET"
+    else
+        if [[ -d "$TARGET" ]]; then
+            echo "!! Backing up old $NAME configuration"
+            mv "$TARGET" "$PWD/old"
+        fi
+    fi
+    ln -s "$SOURCE" "$TARGET"
+    echo ":: Done"
+}
+
+link "$PWD/nvim" "$HOME/.config/nvim" "Neovim"
