746d5969746e0cceade7e3502466d6a38a666a96
Kett Lovahr <catusfelony@gmail.com>
2023-08-31 19:33:29 -0300

    Changed Neovim Gui Font

--- files/nvim/lua/kett/init.lua
+++ files/nvim/lua/kett/init.lua
@@ -24,7 +24,7 @@ vim.opt.updatetime = 100
 
 vim.opt.fillchars= "eob: ,"
 
-vim.opt.guifont = "Fira Code:h12"
+vim.opt.guifont = "Victor Mono:h12"
 
 vim.api.nvim_create_autocmd(
     {"BufEnter"}, {
