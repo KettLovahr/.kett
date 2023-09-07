216507a1464e0245fa54906f43fd6390feb5912c
Kett Lovahr <catusfelony@gmail.com>
2023-09-07 17:43:56 -0300

    Add mapping to open quickfix list

--- files/nvim/lua/kett/remap.lua
+++ files/nvim/lua/kett/remap.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<Leader>xx", vim.cmd.Ex)
 vim.keymap.set("n", "<Leader>xs", vim.cmd.Sex)
 vim.keymap.set("n", "<C-l>", vim.cmd.tabnext)
 vim.keymap.set("n", "<C-h>", vim.cmd.tabprev)
+vim.keymap.set("n", "<Leader>qf", vim.cmd.copen)
 
 vim.keymap.set("n", "<Leader>cd", ":cd %:p:h<CR>:pwd<CR>")
 vim.keymap.set("n", "<Leader>cc", '"+yy') -- Copy current line to system clipboard
