c22bb5979848c58ae1032d5220020b222e0e5e50
Kett Lovahr <catusfelony@gmail.com>
2023-10-20 00:07:21 -0300

    Update telescope to newer version

--- files/nvim/lua/kett/packer.lua
+++ files/nvim/lua/kett/packer.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function(use)
     use 'nvim-lualine/lualine.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
