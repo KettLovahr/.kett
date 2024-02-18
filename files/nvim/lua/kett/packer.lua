4c04d03827be13c10964ecf8f17c84007688ead1
Kett Lovahr <catusfelony@gmail.com>
2024-02-18 01:22:52 -0300

    Tweaks

--- files/nvim/lua/kett/packer.lua
+++ files/nvim/lua/kett/packer.lua
@@ -17,6 +17,37 @@ return require('packer').startup(function(use)
         config = function ()
             require("lualine").setup({
                 theme = 'auto',
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {
+                        'branch',
+                        'diff',
+                        {
+                            'diagnostics',
+                            symbols = {
+                                error = "E",
+                                warn = "W",
+                                info = "I",
+                                hint = "H",
+                            }
+                        }
+                    },
+                    lualine_c = {'filename'},
+                    lualine_x = {
+                        'encoding',
+                        {
+                            'fileformat',
+                            symbols = {
+                                unix = "unix",
+                                dos = "dos",
+                                mac = "mac",
+                            }
+                        },
+                        'filetype'
+                    },
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
             })
         end
     }
