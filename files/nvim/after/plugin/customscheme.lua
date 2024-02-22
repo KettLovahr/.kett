7adad6df066de8de82847b84d1ee45c52f4e51d7
Kett Lovahr <catusfelony@gmail.com>
2024-02-22 02:28:02 -0300

    Change DiagnosticWarn color to something more sensible

--- files/nvim/after/plugin/customscheme.lua
+++ files/nvim/after/plugin/customscheme.lua
@@ -1,14 +1,6 @@
+local hi = require("colorscheme").highlight
+local colors = require('base16-colorscheme').colors
+
 vim.cmd("colorscheme base16-atelier-cave")
 
-vim.cmd("hi Pmenu      guifg=#999999 guibg=#000000")
-vim.cmd("hi PmenuSel   guifg=#EEEEEE guibg=#212121")
-vim.cmd("hi PmenuThumb guifg=#EEEEEE guibg=#434852")
-
-
-vim.cmd("hi! link rainbowcol1 rainbow1")
-vim.cmd("hi! link rainbowcol2 rainbow2")
-vim.cmd("hi! link rainbowcol3 rainbow3")
-vim.cmd("hi! link rainbowcol4 rainbow4")
-vim.cmd("hi! link rainbowcol5 rainbow5")
-vim.cmd("hi! link rainbowcol6 rainbow6")
-vim.cmd("hi! link rainbowcol7 rainbow7")
+hi.DiagnosticWarn = {ctermfg = 3, guifg = colors.base0A}
