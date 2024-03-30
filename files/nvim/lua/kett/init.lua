3ba0315097004613c9bbf0f84bf1203620c16b99
Kett Lovahr <catusfelony@gmail.com>
2024-03-30 02:49:56 -0300

    Set default width and height for open_floating_preview oh god you can override functions

--- files/nvim/lua/kett/init.lua
+++ files/nvim/lua/kett/init.lua
@@ -68,3 +68,12 @@ vim.api.nvim_create_autocmd(
         command = "setlocal nonumber"
     }
 )
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or 'single'
+    opts.max_width= opts.max_width or 80
+    opts.max_height= opts.max_height or 24
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
