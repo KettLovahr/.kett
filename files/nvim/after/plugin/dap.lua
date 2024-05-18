fa612eb9bcbc9aad70d475938e8c0b000d29c011
Kett Lovahr <catusfelony@gmail.com>
2024-05-18 01:58:53 -0300

    Automatically open/close the dap-gui when debugging starts/ends

--- files/nvim/after/plugin/dap.lua
+++ files/nvim/after/plugin/dap.lua
@@ -37,6 +37,7 @@ local dap_ui = require('dapui')
 
 vim.keymap.set("n", "<F5>", dap.continue)
 vim.keymap.set("n", "<F6>", dap.pause)
+vim.keymap.set("n", "<F8>", dap.terminate)
 
 vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
 vim.keymap.set("n", "<Leader>dr", dap.repl.open)
@@ -52,3 +53,13 @@ vim.keymap.set("n", "<F9>", dap.step_over)
 vim.keymap.set("n", "<F10>", dap.step_into)
 vim.keymap.set("n", "<F11>", dap.step_out)
 vim.keymap.set("n", "<F12>", dap.step_back)
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dap_ui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dap_ui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dap_ui.close()
+end
