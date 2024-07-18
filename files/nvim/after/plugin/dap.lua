0952fc67e0b5747ed8f7740278c25196c329e243
KettLovahr <catusfelony@gmail.com>
2024-07-18 00:08:28 -0300

    Changed Step-out mapping as to not conflict with Neovide fullscreen

--- files/nvim/after/plugin/dap.lua
+++ files/nvim/after/plugin/dap.lua
@@ -51,7 +51,7 @@ end)
 
 vim.keymap.set("n", "<F9>", dap.step_over)
 vim.keymap.set("n", "<F10>", dap.step_into)
-vim.keymap.set("n", "<F11>", dap.step_out)
+vim.keymap.set("n", "S-<F10>", dap.step_out)
 vim.keymap.set("n", "<F12>", dap.step_back)
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
