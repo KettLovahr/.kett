addfdd2a8beb7b398b92ee370bb8648cd80e1135
KettLovahr <catusfelony@gmail.com>
2025-02-01 15:59:21 -0300

    Add branch to lsp-zero to enforce consistency

--- files/nvim/lua/kett/packer.lua
+++ files/nvim/lua/kett/packer.lua
@@ -127,6 +127,7 @@ return require('packer').startup(function(use)
 
     use {
         'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
         requires = {
             -- LSP Support
             {'neovim/nvim-lspconfig'},
